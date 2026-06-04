264371519ebbc7be0c98804cea237c6e55907506
Miguel Muniz <mr.muniz4230@gmail.com>
2026-06-03 23:17:21 -0300

    H2 database, test profile, JPA

--- src/main/java/com/miguelmuniz/api/entites/User.java
+++ src/main/java/com/miguelmuniz/api/entites/User.java
@@ -1,8 +1,13 @@
 package com.miguelmuniz.api.entites;
 
-import java.util.Objects;
+import jakarta.persistence.*;
 
+import java.util.Objects;
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
