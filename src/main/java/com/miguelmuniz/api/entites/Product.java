503a35360f00a68a4442a51761d16231ac8cc51b
Miguel Muniz <mr.muniz4230@gmail.com>
2026-06-09 09:51:54 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/miguelmuniz/api/entites/Product.java
+++ src/main/java/com/miguelmuniz/api/entites/Product.java
@@ -1,5 +1,6 @@
 package com.miguelmuniz.api.entites;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -24,6 +25,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -78,6 +82,14 @@ public class Product {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
